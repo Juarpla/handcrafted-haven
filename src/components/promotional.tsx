501a90d9c498654e1022adbefe0512bcbc9685ab
dofelton <90722967+dofelton@users.noreply.github.com>
2024-10-09 12:36:03 -0500

    Promotional section

---
+++ src/components/promotional.tsx
@@ -0,0 +1,21 @@
+import Blacksmith from '/src/public/images/blacksmithProducts.jpg';
+import AztecNecklace from '/src/public/images/aztecNecklace.jpg';
+import HandwovenScarf from '/src/public/images/handwovenScarf.jpg';
+import React from 'react';
+import { GeistVF } from '/src/app/fonts';
+
+export default function Promotional () {
+    return (
+        <div className="w-full md:col-span-3">
+            <h2 className={`${GeistVF.className} mb-4 text-xl md:text-xl`}>
+            Our Best Current Products
+            </h2>
+
+            <div>
+                <Image src={Blacksmith} alt="Image of a variety of blacksmith made products"/>
+                <Image src={AztecNecklace} alt="Image of aztec silver and turqoise necklace"/>
+                <Image src={HandwovenScarf} alt="Image of a scarf"/>
+            </div>
+        </div>
+    )
+} 
