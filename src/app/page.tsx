ef4d184d1ba4ae5005fad2e3976c8e479e77c81a
leoaam24 <102619114+leoaam24@users.noreply.github.com>
2024-10-09 12:08:08 +0800

    added navbar and added some tailwind config

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import HeroSection from '@/components/HeroSection';
+import Navigation from './ui/Navigation';
 
 export default function Home() {
   return (
     <>
     <main>
+      <Navigation />
       <HeroSection />
     </main>
     </>
