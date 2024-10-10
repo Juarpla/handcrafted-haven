54b70e1066fca8fed41e95a83ef203f8e88e725a
dofelton <90722967+dofelton@users.noreply.github.com>
2024-10-10 12:35:50 -0500

    font update

--- src/components/promotional.tsx
+++ src/components/promotional.tsx
@@ -2,12 +2,12 @@ import Blacksmith from '/src/public/images/blacksmithProducts.jpg';
 import AztecNecklace from '/src/public/images/aztecNecklace.jpg';
 import HandwovenScarf from '/src/public/images/handwovenScarf.jpg';
 import React from 'react';
-import { GeistVF } from '/src/app/fonts';
+import { playfairDisplay } from '/src/app/fonts';
 
 export default function Promotional () {
     return (
         <div className="w-full md:col-span-3">
-            <h2 className={`${GeistVF.className} mb-4 text-xl md:text-xl`}>
+            <h2 className={`${playfairDisplay.className} mb-4 text-xl md:text-xl`}>
             Our Best Current Products
             </h2>
 
