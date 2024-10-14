44d34f79c667fc61b4054b5208cbcc8beb2f5b30
leoaam24 <102619114+leoaam24@users.noreply.github.com>
2024-10-15 04:38:18 +0800

    build tsx page

--- src/app/ui/Navigation.tsx
+++ src/app/ui/Navigation.tsx
@@ -1,30 +1,48 @@
 /* eslint-disable */
 
 import Link from "next/link"
+import Image from "next/image"
+import { Button } from "./button"
 
 export default function Navigation() {
 
     return (
-        <div className="flex h-full px-5 my-5 justify-end">
-            <Link className="flex justify-center flex-grow hover:shadow-text-yellow transition duration-300 px-3 py-3 rounded-md"
-            href="/">
-                <p>
-                    Home</p>
-            </Link>
+        <>
+        <nav className="fixed top-0 w-full h-[10%] flex items-center justify-between px-0 py-0 bg-opacity-100 z-50 bg-custom-orange">
+            <div className="flex-shrink-0">
+                <Link
+                href={"/"}>
+                    <Image 
+                    src="https://placehold.co/600x400"
+                    alt="handlecraft-haven-logo"
+                    width={100}
+                    height={80}
+                    className="ml-1 my-0 py-1"
+                />
+                </Link>    
+            </div>
+            <div className="flex flex-shrink-0 h-full py-0 my-0 space-x-0 ">
+                <Link className="flex justify-center items-center transition duration-500 px-3 py-5 text-black font-bold text-xl" href="/">
+                    Products
+                </Link>
+                <Link className="flex justify-center items-center transition duration-500 px-3 py-5 text-black font-bold text-xl" href="/Sellers">
+                    Sellers
+                </Link>
 
-            <Link className="flex justify-center flex-grow hover:shadow-text-yellow transition duration-300 px-3 py-3 rounded-md"
-            href="/">
-              <p>
-              Products</p>
-            </Link>
+                <Link className="flex justify-center transition duration-500 px-1 py-5 text-black font-bold text-xl" href="/dashboard/login">
+                    <Button>
+                        Login
+                    </Button>
+                </Link>
 
-            <Link className="flex justify-center flex-grow hover:shadow-text-yellow transition duration-300 px-3 py-3 rounded-md"
-            href="/dashboard/login/">
-                <p>
-                Login</p>
-            </Link>
-
-        </div>
+                <Link className="flex justify-center transition duration-500 px-1 py-5 text-black font-bold text-xl" href="/dashboard/login">
+                    <Button>
+                        Register
+                    </Button>
+                </Link>
+            </div>
+        </nav>
+    </>
     )
     
 }
