43fd6ff0b6daf50ff5cbdea4059f645c0ebfc825
Juan Plasencia <jplasencia.dev@outlook.com>
2024-10-16 10:13:56 -0500

    ⚙️ Fix next auth connection

--- src/app/api/auth/nextauth.ts
+++ src/app/api/auth/nextauth.ts
@@ -1,15 +1,15 @@
-import NextAuth from 'next-auth';
-import GitHubProvider from 'next-auth/providers/github';
+import NextAuth from "next-auth";
+import GitHubProvider from "next-auth/providers/github";
 
 export const authOptions = {
   providers: [
     GitHubProvider({
-      clientId: process.env.GITHUB_CLIENT_ID,
-      clientSecret: process.env.GITHUB_CLIENT_SECRET,
+      clientId: process.env.GITHUB_CLIENT_ID as string,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET as string,
     }),
   ],
   pages: {
-    signIn: '/login', // custom login page
+    signIn: "/login", // custom login page
   },
 };
 
