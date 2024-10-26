ee2a7962cac149c41167dc9c35bbd1e70fb985b3
leoaam24 <102619114+leoaam24@users.noreply.github.com>
2024-10-26 22:32:47 +0800

    css and formatting

--- src/app/products/products.tsx
+++ src/app/products/products.tsx
@@ -6,12 +6,12 @@ import { Product } from "../lib/definitions";
 export default function ProductsTable({productData,}:{productData:Product[]}) {
     return (
         <>
-        <div>
+        <div className="flex flex-col">
             <p>{productData?.map((data)=> (
-                <div key={data.id} className="display-block border-black-500">
+                <div key={data.id} className="block rounded-md gap-4 border border-black-1000 p-4 m-2 ">
                     <p className="text-black">{data.productname}</p>
-                    <p className="text-black">${data.price}</p>
-                    <p className="text-black">{data.description}</p>
+                    <p className="text-black">$ {data.price}</p>
+                    <p className="text-black">Description: {data.description}</p>
                 </div>
             ))}</p>
         </div>
